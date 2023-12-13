e621b36a81174a6990dae5ff771c03e2fcb3604f
Thabo <thabo.radebe@live.com>
2023-12-13 23:00:39 +0200

    thirdline

--- src/components/CashBook/CashBook.jsx
+++ src/components/CashBook/CashBook.jsx
@@ -21,20 +21,26 @@ const CashBook = () => {
         </div>
 
         <div className={classes.secondRow}>
-        <div className={classes.receivedFromLabel}>RECEIVED FROM : </div>
-                  <select id="province" className={classes.provinceControlClasses}>
-                    <option value="">Select Province</option>
-                    <option value="Eastern Cape">Eastern Cape</option>
-                    <option value="Free State">Free State</option>
-                    <option value="Gauteng">Gauteng</option>
-                    <option value="KwaZulu-Natal">KwaZulu-Natal</option>
-                    <option value="Limpopo">Limpopo</option>
-                    <option value="Mpumalanga">Mpumalanga</option>
-                    <option value="North West">North West</option>
-                    <option value="Northern Cape">Northern Cape</option>
-                    <option value="Western Cape">Western Cape</option>
-                  </select>
-        <input type="text" id="Branch" placeholder="Branch" className={classes.input} />
+          <div className={classes.receivedFromLabel}>RECEIVED FROM : </div>
+                    <select id="province" className={classes.provinceControlClasses}>
+                      <option value="">Select Province</option>
+                      <option value="Eastern Cape">Eastern Cape</option>
+                      <option value="Free State">Free State</option>
+                      <option value="Gauteng">Gauteng</option>
+                      <option value="KwaZulu-Natal">KwaZulu-Natal</option>
+                      <option value="Limpopo">Limpopo</option>
+                      <option value="Mpumalanga">Mpumalanga</option>
+                      <option value="North West">North West</option>
+                      <option value="Northern Cape">Northern Cape</option>
+                      <option value="Western Cape">Western Cape</option>
+                    </select>
+          <input type="text" id="Branch" placeholder="Branch" className={classes.input} />
+        </div>
+        <div className={classes.thirddRow}>
+          <span className={classes.receivedFromLabel}>THE SUM OF :   </span>
+          <input type="text" id="Branch" placeholder="Branch" className={classes.input} />
+        
+          
         </div>
   
       </div>
