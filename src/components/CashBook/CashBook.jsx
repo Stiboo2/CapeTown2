988f95a26185ad5cc38b80c245153376b17cde3c
Thabo <thabo.radebe@live.com>
2023-12-20 23:05:28 +0200

    Other_Option

--- src/components/CashBook/CashBook.jsx
+++ src/components/CashBook/CashBook.jsx
@@ -1,22 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect  } from 'react';
 import classes from './CashBook.module.css'; // Adjust the import path
+
 const CashBook = () => {
   const imageUrl = 'https://res.cloudinary.com/dkayrcyeb/image/upload/v1700912962/church/hkj7nwjctabyzrsvhtbj.png';
-    const [selectedOptions, setSelectedOptions] = useState([]);
+  const [selectedOptions, setSelectedOptions] = useState([]);
+  const [selectedOtherOptions, setSelectedOtherOptions] = useState(false);
+  const [otherInputValue, setOtherInputValue] = useState('');
+  
+  const handleOptionChange = (e) => {
+    const option = e.target.value;
 
-    const handleOptionChange = (e) => {
-      const option = e.target.value;
+    if (option === 'Other') {
+      // Toggle the input field for "Other" when selected
+      setSelectedOtherOptions((prev) => !prev);
+       } 
+      // If the option is not "Other", handle as before
       if (selectedOptions.includes(option)) {
-        // If the option is already selected, remove it
         setSelectedOptions(selectedOptions.filter((selected) => selected !== option));
       } else {
-        // If the option is not selected, add it
         setSelectedOptions([...selectedOptions, option]);
       }
-    };
+    
+  };
+  const handleOtherInputChange = (e) => {
+    // Update the value of the "Other" input field
+    setOtherInputValue(e.target.value);
+  };
 
-
- return (
+  return (
     <div className={classes.container}>
       <div className={classes.mainBox}>
         <div className={classes.firstRows}>
@@ -35,81 +46,89 @@ const CashBook = () => {
 
         <div className={classes.secondRow}>
           <div className={classes.receivedFromLabel}>RECEIVED FROM : </div>
-                    <select id="province" className={classes.provinceControlClasses}>
-                      <option value="">Select Province</option>
-                      <option value="Eastern Cape">Eastern Cape</option>
-                      <option value="Free State">Free State</option>
-                      <option value="Gauteng">Gauteng</option>
-                      <option value="KwaZulu-Natal">KwaZulu-Natal</option>
-                      <option value="Limpopo">Limpopo</option>
-                      <option value="Mpumalanga">Mpumalanga</option>
-                      <option value="North West">North West</option>
-                      <option value="Northern Cape">Northern Cape</option>
-                      <option value="Western Cape">Western Cape</option>
-                    </select>
+          <select id="province" className={classes.provinceControlClasses}>
+            <option value="">Select Province</option>
+            <option value="Eastern Cape">Eastern Cape</option>
+            <option value="Free State">Free State</option>
+            <option value="Gauteng">Gauteng</option>
+            <option value="KwaZulu-Natal">KwaZulu-Natal</option>
+            <option value="Limpopo">Limpopo</option>
+            <option value="Mpumalanga">Mpumalanga</option>
+            <option value="North West">North West</option>
+            <option value="Northern Cape">Northern Cape</option>
+            <option value="Western Cape">Western Cape</option>
+          </select>
           <input type="text" id="Branch" placeholder="Branch" className={classes.input} />
         </div>
+
         <div className={classes.thirdRow}>
-           
-      <span className={classes.receivedFromLabel}>EXPENDITURE: </span>
+          <span className={classes.receivedFromLabel}>EXPENDITURE: </span>
 
-      {/* Use checkboxes for the options */}
-      <label>
-        <input
-          type="checkbox"
-          value="Rent"
-          checked={selectedOptions.includes('Rent')}
-          onChange={handleOptionChange}
-        />
-        Rent
-      </label>
+          <label>
+            <input
+              type="checkbox"
+              value="Rent"
+              checked={selectedOptions.includes('Rent')}
+              onChange={handleOptionChange}
+            />
+            Rent
+          </label>
 
-      <label>
-        <input
-          type="checkbox"
-          value="HolyCommunion"
-          checked={selectedOptions.includes('HolyCommunion')}
-          onChange={handleOptionChange}
-        />
-        Holy Communion
-      </label>
+          <label>
+            <input
+              type="checkbox"
+              value="HolyCommunion"
+              checked={selectedOptions.includes('HolyCommunion')}
+              onChange={handleOptionChange}
+            />
+            Holy Communion
+          </label>
 
-      <label>
-        <input
-          type="checkbox"
-          value="Transport"
-          checked={selectedOptions.includes('Transport')}
-          onChange={handleOptionChange}
-        />
-        Transport
-      </label>
+          <label>
+            <input
+              type="checkbox"
+              value="Transport"
+              checked={selectedOptions.includes('Transport')}
+              onChange={handleOptionChange}
+            />
+            Transport
+          </label>
 
-      <label>
-        <input
-          type="checkbox"
-          value="Other"
-          checked={selectedOptions.includes('Other')}
-          onChange={handleOptionChange}
-        />
-        Other
-      </label>
-
-      {/* Display the selected options (optional) */}
-      {selectedOptions.length > 0 && (
-        <p>Selected Options: {selectedOptions.join(', ')}</p>
-      )}
-    
+          <label>
+            <input
+              type="checkbox"
+              value="Other"
+              checked={selectedOptions.includes('Other') || selectedOtherOptions}
+              onChange={handleOptionChange}
+            />
+            Other
+          </label>
         </div>
-            <div className={classes.fourthRow}>
+       {selectedOtherOptions && (
+            <input
+              type="text"
+              id="Other"
+              placeholder="Please specify"
+              className={classes.input}
+              value={otherInputValue}
+              onChange={handleOtherInputChange}
+            />
+          )}
+        <div className={classes.fourthRow}>
           <span className={classes.receivedFromLabel}>RECIEVED BY :   </span>
           <input type="text" id="Branch" placeholder="Branch" className={classes.input} />
           <span className={classes.receivedFromLabel}>SIGN BY :   </span>
           <input type="text" id="Branch" placeholder="Branch" className={classes.input} />
         </div>
-  
+        {selectedOptions.length > 0 && (
+          <p>
+            Selected Options: {selectedOptions.includes('Other') ? selectedOptions.filter(option => option !== 'Other').join(', ') : selectedOptions.join(', ')}
+            {selectedOptions.includes('Other') && `, Other: ${otherInputValue}`}
+          </p>
+        )}
       </div>
     </div>
   );
-}
+};
 
 export default CashBook;
